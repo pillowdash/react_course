69a962b64a603cbbf9d0bf89f3168fbceb8cf076
Tiger W <comefromtoronto@gmail.com>
2026-01-22 22:38:52 -0500

    move all pages into react, split header into component, use react-router <Route> and <Link> components

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,20 @@
-
+import { Routes, Route} from 'react-router'
 import { HomePage } from './Pages/HomePage.jsx'
+import { CheckoutPage } from './Pages/Checkout.jsx'
+import { OrderPage } from './Pages/OrderPage.jsx'
+import { TrackingPage } from './Pages/TrackingPage.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage />}></Route>
+        <Route path="checkout" element={<CheckoutPage />}></Route>
+        <Route path="orders" element={<OrderPage />}></Route>
+        <Route path="tracking" element={<TrackingPage />}></Route>
+      </Routes>
     </>
   )
 }
